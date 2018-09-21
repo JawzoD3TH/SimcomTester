bd874f725c93fe484c0282d84b49263cb140f00e
Jawid Hassim <hassimjawid@gmail.com>
2018-09-21 17:19:51 +0200

    First

--- ConsoleApp1/Properties/AssemblyInfo.cs
+++ ConsoleApp1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp1")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SimcomTester")]
+[assembly: AssemblyDescription("Basically Dial / SMS using a Simcom Board Over TTL")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyCompany("Communica (Pty) Ltd")]
+[assembly: AssemblyProduct("SimcomTester")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
